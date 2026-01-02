affcd24dee156d026df3a7e776cd14155a874f23
{bigfive} <bigfive0907@gmail.com>
2026-01-02 18:58:34 +0900

    TwoSumをO(N)で解けるように修正した

--- problems/easy/two_sum_0001.py
+++ problems/easy/two_sum_0001.py
@@ -1,9 +1,29 @@
 from utils import *
 
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        length = len(nums)
+        answer = {}
+
+        for i in range(length):
+            compl = target - nums[i]
+
+            if compl in answer:
+                return [answer[compl], i]
+            else:
+                answer[nums[i]] = i
+
+        # length = len(nums)
+
+        # for i in range(length):
+        #     for j in range(i+1, length):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        #         else:
+        #             next
